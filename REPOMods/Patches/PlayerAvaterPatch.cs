c8a9350ba68aef0fd1910e43fcca5e6305e29459
Jglauby <jglauby@gmail.com>
2025-03-16 15:54:10 -0500

    Add project files.

---
+++ REPOMods/Patches/PlayerAvaterPatch.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+
+namespace OpJosModREPO.modname.Patches
+{
+    [HarmonyPatch(typeof(PlayerAvatar))]
+    internal class PlayerAvaterPatch
+    {
+        private static ManualLogSource mls;
+        public static void SetLogSource(ManualLogSource logSource)
+        {
+            mls = logSource;
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void UpdatePatch(PlayerAvatar __instance)
+        {
+            if (SemiFunc.InputDown(InputKey.Jump))
+            {
+                mls.LogMessage("Jump pressed");
+            }
+        }
+    }
+}
---
+++ REPOMods/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using OpJosModREPO.modname.Patches;
+
+namespace OpJosModREPO.modname
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class OpJosModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "OpJosModREPO.modname";
+        private const string modName = "modname";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmoy = new Harmony(modGUID);
+        private static OpJosModBase Instance;
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo($"{modName} has started!");
+
+            PlayerAvaterPatch.SetLogSource(mls);
+            harmoy.PatchAll();
+        }
+    }
+}
