252d2676e35abb4022e2ff1fb2a293d1ab61739e
Jglauby <jglauby@gmail.com>
2025-03-16 16:26:46 -0500

    more seutp

--- REPOMods/Patches/PlayerAvaterPatch.cs
+++ REPOMods/Patches/PlayerAvaterPatch.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx.Logging;
 using HarmonyLib;
+using UnityEngine;
 
 namespace OpJosModREPO.Tourettes.Patches
 {
@@ -12,14 +13,25 @@ namespace OpJosModREPO.Tourettes.Patches
             mls = logSource;
         }
 
+        private static float nextExecutionTime = 0f;
+        private static System.Random rng = new System.Random();
+
         [HarmonyPatch("Update")]
         [HarmonyPostfix]
         static void UpdatePatch(PlayerAvatar __instance)
         {
-            if (SemiFunc.InputDown(InputKey.Jump))
+            if (Time.time > nextExecutionTime)
             {
-                mls.LogMessage("Jump pressed");
+                mls.LogInfo("said random phrase!");
+                sayPhrase();
+
+                nextExecutionTime = Time.time + rng.Next(1 * 60, 6 * 60);
             }
+        }
+
+        static void sayPhrase()
+        {
+
         }
     }
 }
---
+++ REPOMods/Phrases.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REPOMods
+{
+    public class Phrases
+    {
+        private static readonly List<string> phraseList = new List<string>
+        {
+            "Hello, world!",
+            "Stay positive!",
+            "Keep moving forward!",
+            "Never give up!"
+        };
+        private static readonly Random rng = new Random();
+
+        public static string GetRandomPhrase()
+        {
+            return phraseList[rng.Next(phraseList.Count)];
+        }
+    }
+}
