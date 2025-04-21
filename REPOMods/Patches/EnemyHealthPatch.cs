52066d8a3b7f3b5e3f53bb485b4daac4a0fccfa3
Jglauby <jglauby@gmail.com>
2025-04-21 17:24:42 -0500

    Fix lag when attacking other non controlled ducks

--- REPOMods/Patches/EnemyHealthPatch.cs
+++ REPOMods/Patches/EnemyHealthPatch.cs
@@ -20,6 +20,7 @@ namespace OpJosModREPO.IAmDucky.Patches
         static void DeathRPCPatch(EnemyHealth __instance)
         {
             Enemy enemy = ReflectionUtils.GetFieldValue<Enemy>(__instance, "enemy");
+            if (enemy == null) return;
             EnemyDuck duck = enemy.GetComponent<EnemyDuck>();
             if (duck == null) return; //not duck that died
 
@@ -30,6 +31,12 @@ namespace OpJosModREPO.IAmDucky.Patches
             }
 
             DuckPlayerController ducksController = GeneralUtil.FindDuckController(duck);
+            if (ducksController == null)
+            {
+                mls.LogWarning("No DuckPlayerController found for duck. Skipping DeathRPC handling.");
+                return;
+            }
+
             if (PhotonNetwork.LocalPlayer.ActorNumber == ducksController.controlActorNumber && ReflectionUtils.GetFieldValue<bool>(PlayerAvatar.instance, "deadSet")) //is your duck
             {
                 mls.LogInfo("Duck dying is duck being controlled, release control of duck");
