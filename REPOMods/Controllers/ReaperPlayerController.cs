a293fc0509ad53cb4d88782e5f9871566c993e72
Jglauby <jglauby@gmail.com>
2025-06-20 00:59:11 -0500

    reaper can now hurt other enemies

--- REPOMods/Controllers/ReaperPlayerController.cs
+++ REPOMods/Controllers/ReaperPlayerController.cs
@@ -1,6 +1,9 @@
 ﻿using OpJosModREPO.IAmEnemy;
 using OpJosModREPO.IAmEnemy.Util;
 using Photon.Pun;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace OpJosModREPO.Controllers.IAmEnemy
 {
@@ -30,12 +33,78 @@ namespace OpJosModREPO.Controllers.IAmEnemy
                 return;
 
             base.FixedUpdateLogic();
+
+            if (thisRunner.currentState == EnemyRunner.State.Idle)
+                ReflectionUtils.InvokeMethod(thisRunner, "UpdateState", new object[] { EnemyRunner.State.Roam });
+
+            if (isYourEnemy)
+            {
+                if (attackCooldown > 0f)
+                    attackCooldown -= Time.fixedDeltaTime;
+
+                if (attackCooldown <= 0f)
+                {
+                    attackNearbyEnemies();
+                    attackCooldown = 0.75f;
+                }
+            }
         }
 
         private void handleInput()
         {
             if (controlActorNumber != PhotonNetwork.LocalPlayer.ActorNumber)//dont listen to keys if not your enemy
                 return;
-        }  
+
+            try
+            {
+                if (Keyboard.current[ConfigVariables.attackButtonKey].wasPressedThisFrame)
+                {
+                    if (thisRunner.currentState == EnemyRunner.State.AttackPlayer)
+                    {
+                        mls.LogInfo("Stopping reaper attack mode");
+                        ReflectionUtils.InvokeMethod(thisRunner, "UpdateState", new object[] { EnemyRunner.State.Idle });
+                    }
+                    else if (ConfigVariables.allowAttackToggle)
+                    {
+                        mls.LogInfo("Starting reaper attack mode");
+                        ReflectionUtils.InvokeMethod(thisRunner, "UpdateState", new object[] { EnemyRunner.State.AttackPlayer });
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void attackNearbyEnemies()
+        {
+            if (thisRunner.currentState == EnemyRunner.State.AttackPlayer)
+            {
+                List<Enemy> closeEnemies = GeneralUtil.FindCloseEnemies(thisRunner.transform.position, 3.5f);
+                foreach (var enemy in closeEnemies)
+                {
+                    if (enemy != null && enemy.GetInstanceID() != thisRunner.enemy.GetInstanceID())//not controlled enemy
+                    {
+                        Vector3 toEnemy = (enemy.transform.position - thisRunner.transform.position).normalized;
+                        float angle = Vector3.Angle(thisRunner.transform.forward, toEnemy);
+
+                        if (angle < 50f)
+                        {
+                            EnemyHealth healthComponent = ReflectionUtils.GetFieldValue<EnemyHealth>(enemy, "Health");
+                            if (healthComponent != null)
+                            {
+                                // Get direction from this enemy to enemy beign attacked
+                                Vector3 hurtDir = (enemy.transform.position - thisRunner.transform.position).normalized;
+
+                                // Call internal method "Hurt"
+                                healthComponent.Hurt(40, hurtDir);
+                            }
+                            else
+                            {
+                                mls.LogError($"Health component not found for enemy: {enemy.name}");
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
