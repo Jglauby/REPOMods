64440bb4b5ff7f3f57c5682ce0a930f19f95a1a0
Jglauby <jglauby@gmail.com>
2025-06-19 21:17:36 -0500

    mentalist attack kinda works

--- REPOMods/Controllers/MentalistPlayerController.cs
+++ REPOMods/Controllers/MentalistPlayerController.cs
@@ -1,6 +1,9 @@
 ﻿using OpJosModREPO.IAmEnemy;
 using OpJosModREPO.IAmEnemy.Util;
 using Photon.Pun;
+using System;
+using UnityEngine.InputSystem;
+using UnityEngine;
 
 namespace OpJosModREPO.Controllers.IAmEnemy
 {
@@ -32,10 +35,33 @@ namespace OpJosModREPO.Controllers.IAmEnemy
             base.FixedUpdateLogic();
         }
 
-        private void handleInput()
+        private void handleInput()  
         {
             if (controlActorNumber != PhotonNetwork.LocalPlayer.ActorNumber)//dont listen to keys if not your enemy
                 return;
+
+            try
+            {
+                if (Keyboard.current[ConfigVariables.attackButtonKey].wasPressedThisFrame && ConfigVariables.allowAttackToggle)
+                {
+                    if (thisMentalist.currentState == EnemyFloater.State.Attack)
+                    {
+                        mls.LogInfo("Stopping mentalist attack");
+                        ReflectionUtils.InvokeMethod(thisMentalist, "UpdateState", new object[] { EnemyFloater.State.Idle });
+                    }
+                    else
+                    {
+                        mls.LogInfo("Starting mentalist attack");
+                        ReflectionUtils.InvokeMethod(thisMentalist, "UpdateState", new object[] { EnemyFloater.State.ChargeAttack });
+
+                        DelayUtility.RunAfterDelay(0.5f, () =>
+                        {
+                            ReflectionUtils.InvokeMethod(thisMentalist, "UpdateState", new object[] { EnemyFloater.State.Attack });
+                        });
+                    }
+                }
+            }
+            catch { }
         }  
     }
 }
